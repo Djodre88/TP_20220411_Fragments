40efb31f3f699042c21a40be52c535df16d06fc3
Djodre88 <jodre.diangienda@gmail.com>
2022-04-11 10:09:08 +0200

    Création des fragments + implémentation des méthodes du cycle de vie du fragment1

--- app/src/main/java/com/example/tp_20220411_fragments/Fragment1.java
+++ app/src/main/java/com/example/tp_20220411_fragments/Fragment1.java
@@ -1,12 +1,18 @@
 package com.example.tp_20220411_fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class Fragment1 extends Fragment {
@@ -19,6 +25,64 @@ public class Fragment1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_1, container, false);
+        View view = inflater.inflate(R.layout.fragment_1, container, false);
+        Button button = view.findViewById(R.id.button);
+        Log.d("Fragment1", "View créée");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Fallait aller voter !", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("Fragment 1", "Fragment créé");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d("Fragment1", "Le fragment a démarré");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d("Fragment1", "Le fragment a repris");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d("Fragment1", "Le fragment a stoppé");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("Fragment1", "Le fragment est détruit");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d("Fragment1", "Le fragment est en pause");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d("Fragment1", "La View est détruite");
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Log.d("Fragment1", "Le fragment est attaché à l'activité");
     }
 }
